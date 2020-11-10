bd0bcedf52477338aae013dc9cecf2c9097504b4
Haoran Lin <tdkgn@outlook.com>
2020-11-10 23:08:05 +0800

    pass practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,5 @@
-const parseData
-export { parseData };
+export const parseData = ({ data, column }) => {
+  const parseRecord = record =>
+    Object.fromEntries(column.map(({ name }, index) => [name, record[index]]));
+  return data.map(parseRecord);
+};
